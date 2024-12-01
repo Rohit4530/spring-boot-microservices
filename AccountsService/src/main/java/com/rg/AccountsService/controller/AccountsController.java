30af5d0b3fb12b73411444283cfe3b25c85a1c55
rohit9893945 <rohitgunjal547@gmail.com>
2024-12-01 11:39:07 +0000

    added validations

--- AccountsService/src/main/java/com/rg/AccountsService/controller/AccountsController.java
+++ AccountsService/src/main/java/com/rg/AccountsService/controller/AccountsController.java
@@ -4,20 +4,24 @@ import com.rg.AccountsService.Constants.AccountConstant;
 import com.rg.AccountsService.dto.CustomerDTO;
 import com.rg.AccountsService.dto.ResponseDTO;
 import com.rg.AccountsService.service.IAccountService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/api",produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
 public class AccountsController {
     @Autowired
     private IAccountService accountService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO) {
+    public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
         accountService.createAccount(customerDTO);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -27,7 +31,9 @@ public class AccountsController {
                 ));
     }
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDTO>fetchAccountDetails(@RequestParam("mobileNumber") String mobileNumber) throws Exception {
+    public ResponseEntity<CustomerDTO>fetchAccountDetails( @RequestParam("mobileNumber")
+                                                               @Pattern(regexp = "[0-9]{10}",message = "mobile number should be of 10 digits")
+                                                               String mobileNumber) throws Exception {
        CustomerDTO customerDTO= accountService.fetchAccountDetails(mobileNumber);
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -35,7 +41,7 @@ public class AccountsController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDTO> updateAccount(@RequestBody CustomerDTO customerDTO) throws Exception {
+    public ResponseEntity<ResponseDTO> updateAccount(@Valid @RequestBody CustomerDTO customerDTO) throws Exception {
         boolean isUpdated = accountService.updateAccount(customerDTO);
         if (isUpdated) {
             return ResponseEntity
@@ -54,7 +60,10 @@ public class AccountsController {
         }
     }
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDTO> deleteAccount(@RequestParam("mobileNumber") String mobileNumber) throws Exception {
+    public ResponseEntity<ResponseDTO> deleteAccount(@RequestParam("mobileNumber")
+                                                         @Pattern(regexp = "[0-9]{10}",message = "mobile number should be of 10 digits")
+                                                         String mobileNumber) throws Exception {
+
         boolean isDeleted = accountService.deleteAccount(mobileNumber);
         if (isDeleted) {
             return ResponseEntity
--- AccountsService/src/main/java/com/rg/AccountsService/dto/CustomerDTO.java
+++ AccountsService/src/main/java/com/rg/AccountsService/dto/CustomerDTO.java
@@ -1,5 +1,9 @@
 package com.rg.AccountsService.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDTO {
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 2,max = 20,message = "name should be of 2 to 20 characters")
     private String name;
+    @NotEmpty(message = "email should not be empty")
+    @Email
     private  String email;
+   @Pattern(regexp = "[0-9]{10}",message = "mobile number should be of 10 digits")
     private  String mobileNumber;
     private AccountDTO accountDTO;
 }
