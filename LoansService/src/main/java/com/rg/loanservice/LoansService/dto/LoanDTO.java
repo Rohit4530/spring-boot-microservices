94492d4c4fac9625421f4e21b01a88784a2da4d2
rohit9893945 <rohitgunjal547@gmail.com>
2024-12-04 13:00:28 +0000

    added loans-microservice

--- LoansService/src/main/java/com/rg/loanservice/LoansService/dto/LoanDTO.java
+++ LoansService/src/main/java/com/rg/loanservice/LoansService/dto/LoanDTO.java
@@ -1,5 +1,8 @@
 package com.rg.loanservice.LoansService.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoanDTO {
+    @NotEmpty(message = "Mobile number cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number should be of 10 digit")
     private  String mobileNumber;
+    @NotEmpty(message = "Loan type cannot be empty")
     private String loanType;
+    @NotEmpty(message = "Loan number cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{12})",message = "Mobile number should be of 12 digit")
     private String loanNumber;
+    @PositiveOrZero
     private int totalLoan;
+    @PositiveOrZero
     private int amountPaid;
+    @PositiveOrZero
     private int outstandingAmount;
 }
